2176b5f9770fe00a0da0cad3e3acfb236dec8e87
VordaNick <vordanick@gmail.com>
2024-11-09 11:34:13 +0100

    added

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,3 +1,11 @@
-author = Author.objects.get(id=1)
+author = Author.objects.get(name=author_name)
 Books = author.books.all()
 print(Books)
+
+Library = Library.objects.get(name=library_name)
+Books = library.books.all()
+print(Books)
+
+Librarian = library.objects.get(name=library_name)
+Librarian = library.librarian
+print(Librarian)
