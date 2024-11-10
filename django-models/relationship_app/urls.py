7faff592b25b729bd8217880231944a9feb9440d
VordaNick <vordanick@gmail.com>
2024-11-10 18:43:56 +0100

    added

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
      path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
-    path('add-book/', views.add_book_view, name='add_book_view'),
-    path('change-book/', views.change_book_view, name='change_book_view'),
-    path('delete-book/', views.delete_book_view, name='delete_book_view'),
+    path('add_book/', views.add_book_view, name='add_book_view'),
+    path('edit_book/', views.change_book_view, name='change_book_view'),
+    path('delete_book/', views.delete_book_view, name='delete_book_view'),
 
 ]
