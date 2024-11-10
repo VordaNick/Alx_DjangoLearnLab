c16b2963456a61e1a17ba093b146f64e50ff9fd8
VordaNick <vordanick@gmail.com>
2024-11-10 14:53:16 +0100

    added

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
+
 # Create your views here.
 def database_books(request):
     book = Book.objects.all()
@@ -29,4 +31,27 @@ class LibraryBooks(DetailView):
         else:
             form = UserCreationForm()
         return render(request, 'relationship_app/register.html', {'form': form})
+    
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
+
             
