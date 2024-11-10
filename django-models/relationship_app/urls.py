5c0124bba32662fe1799a35cb75296edcedaa4ed
VordaNick <vordanick@gmail.com>
2024-11-10 13:17:04 +0100

    added

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from . import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('database_books/', views.database_books, name='database_books'),
     path('library/<int:library_name>/books/', views.LibraryBooks.as_view(), name='library_detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 # Create your views here.
 def database_books(request):
     book = Book.objects.all()
     context = {
-        'books' : books,
+        'books' : Book
     }
     return(render, 'relationship_app/list_books.html', context)
 
@@ -16,5 +19,14 @@ class LibraryBooks(DetailView):
     context_object_name = 'books'
     def library_name(self):
         library = self.kwargs['library_name']
-        return Book.objects.filter(library_name=library_name)
-    
+        return Book.objects.filter(library_name='library_name')
+    def register(request):
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+        else:
+            form = UserCreationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+            
