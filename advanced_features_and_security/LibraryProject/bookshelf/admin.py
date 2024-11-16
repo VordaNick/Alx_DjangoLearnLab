bc7839f2fc9e70a2bf2318b7be6d5bdc3eed8ead
VordaNick <vordanick@gmail.com>
2024-11-16 13:55:21 +0100

    added

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
 from .models import Book
 "register", "admin.ModelAdmin"
 "list_filter", "author", "publication_year"
 "search_fields", "title"
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
