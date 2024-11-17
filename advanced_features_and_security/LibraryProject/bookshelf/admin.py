b203261834b6410e957d8b844a2bdfab5c26fcba
VordaNick <vordanick@gmail.com>
2024-11-17 14:48:55 +0100

    added

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from .admin import CustomUserAdmin
+from django.contrib.auth.models import User, Group
 
 # Register your models here.
 from .models import Book
@@ -14,3 +15,8 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+user = User.objects.get(username='Vorda')
+group = Group.objects.get(name='Editors')
+user.groups.add(group)
+print(f"{user.username} has been added to {group.name} group")
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
 
 # Create your views here.
+permission_required('bookshelf.can_view', raise_exception=True)
+def view_book(request):
+    return HttpResponse("You have permission to view Book.")
+permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    return HttpResponse("You have permission to craete Book.")
+permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request):
+    return HttpResponse("You have permission to edit Book.")
+permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request):
+    return HttpResponse("You have permission to delete Book.")
