a4618148a5ddf3ae49a5dabfba318e28f26595d1
VordaNick <vordanick@gmail.com>
2024-11-17 15:30:17 +0100

    added

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
+from .forms import ExampleForm
 
 # Create your views here.
 permission_required('bookshelf.can_view', raise_exception=True)
