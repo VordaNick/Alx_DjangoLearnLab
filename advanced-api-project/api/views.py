e6c4610b3d0dfbf07de7fcb6bc23ceb02a472db8
VordaNick <vordanick@gmail.com>
2024-11-28 15:47:59 +0100

    Added

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from .models import Book
 from django.urls import reverse_lazy
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAuthenticated
 from django_filters import rest_framework
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import filters
 from rest_framework import generics
 from .serializers import BookSerializer
 from .filters import BookFilter
@@ -37,10 +37,11 @@ class BookDeleteView(IsAuthenticated,DeleteView):
     template_name = 'Delete.html'
     success_url = reverse_lazy('List')
     
-class BookListApiView(ListAPIView):
+class BookListApiView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    order = filters.OrderingFilter
+    search = filters.SearchFilter
     
     filterset_class = BookFilter
     search_fields = ['title', 'author']
