5c9597ba73e41c064d9b363a36d56d8f91c27d29
VordaNick <vordanick@gmail.com>
2024-12-02 10:15:14 +0100

    Add

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
-User=get_user_model()
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Post(models.Model):
