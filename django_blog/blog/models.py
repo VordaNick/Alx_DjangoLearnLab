27d60ce1e07da81bbe4a4fa09bd80811ae78ab06
VordaNick <vordanick@gmail.com>
2024-12-05 13:34:14 +0100

    Add

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    tags = TaggableManager
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -24,3 +26,6 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+    
+class Tag(models.Model):
+    
