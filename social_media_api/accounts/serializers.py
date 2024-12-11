33caef244a1084258be6895e1070134bcc16c2d6
VordaNick <vordanick@gmail.com>
2024-12-11 15:11:18 +0100

    Added

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,14 +12,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password', "token"]
     def create(self, validated_data):
-        user = User.objects.create_user(email=validated_data['email'], password=validated_data['password'], username=validated_data('username'),)
+        user = get_user_model().objects.create_user(email=validated_data['email'], password=validated_data['password'], username=validated_data('username'),)
         token, created = Token.objects.create(user=user)
         user.token = token.key
         return user
     
 class UserLoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField()
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
     token = serializers.CharField(read_only=True)
     
     def validate(self, data):
