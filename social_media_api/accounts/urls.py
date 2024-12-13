c602507e1e00bb5def1cba8fd91bdac438d6e0cd
VordaNick <vordanick@gmail.com>
2024-12-13 11:03:51 +0100

    Added

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', views.UserRegistrationView.as_view(), name='register')
+    path('register/', views.UserRegistrationView.as_view(), name='register'),
+    path('follow/<int:user_id>/', views.FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', views.UnfollowUserView.as_view(), name='unfollow-user')
 ]
